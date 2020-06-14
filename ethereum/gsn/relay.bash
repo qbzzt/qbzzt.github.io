e9e8b38212ab45d06f89e5511ad902625665e1f9
qbzzt <qbzzt1@gmail.com>
2020-06-14 15:13:00 -0500

    Update relay.bash

--- ethereum/gsn/relay.bash
+++ ethereum/gsn/relay.bash
@@ -7,7 +7,7 @@ docker run --rm \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -v "\$PWD:\$PWD" \
         -w "\$PWD" \
-        docker/compose:1.24.0 $*
+        docker/compose:1.24.0 \$\*
 
 END
 chmod +x docker-compose.bash
