bab0bea056302c5c4804b16a14b59cc3cdc3947e
ChayaJackleywkw67 <ChayaJackleywkw67@gmail.com>
2022-08-28 07:24:21 +0000

    week8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -11,33 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 module.exports = {
   solidity: "0.8.4",
-/*
+
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
-    "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
+    "goerli": {
+       url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+       accounts: ["56ff5f24ad152eb16c5e722e9537e58324617c2c39044f20f20a5e8841a2fac8"]
     }
   }
-*/
+
 };
